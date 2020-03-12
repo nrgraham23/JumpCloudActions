ba2839818cebb6ddc74e24cb22e42b8721f50935
Nathaniel Graham <nrgraham23@gmail.com>
2020-03-11 22:51:54 -0700

    Add an example usage script
    Since tests are done using mocha, convert the previous test
    script to an example usage script.

--- ActionAveragerExampleUsage.js
+++ ActionAveragerExampleUsage.js
@@ -1,15 +1,20 @@
 const ActionAverager = require('./ActionAverager');
 
+// Actions to add
 const action1 = '{"action":"jump", "time":100}';
 const action2 = '{"action":"run", "time":75}';
 const action3 = '{"action":"jump", "time":200}';
-const action4 = '{"time":200}';
 
 const averager = new ActionAverager();
 
+console.log('Adding action: ' + action1);
 averager.addAction(action1);
+console.log('Adding action: ' + action2);
 averager.addAction(action2);
+console.log('Adding action: ' + action3);
 averager.addAction(action3);
-//averager.addAction(action4);
+
+// Get the stats for the actions added
+const stats = averager.getStats();
 
 console.log('Stats are: ' + averager.getStats());
